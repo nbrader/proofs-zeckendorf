d8341b09fd06c4e6e4205094369dacd0b085ab41
nbrader <nbrader+github@gmail.com>
2025-10-27 21:12:38 +0000

    Added further reasoning to my new proof attempt

--- Coq/zeckendorf2.v
+++ Coq/zeckendorf2.v
@@ -26,6 +26,22 @@ Fixpoint zeck_lists (n : nat) : list (list nat) :=
       end
   end.
 
+(*
+  It should be very easy to prove the zeckendorf theorem using zeck which is implemented with zeck_lists.
+
+  1.
+  The zeckendorf representation only includes fibonacci numbers because zeck_lists only adds fibonacci numbers.
+
+  2.
+  The zeckendorf representation only includes non-consecutive fibonacci numbers because:
+  - The largest fibonacci number is only consed on to lists that themselves are non-consecutive and
+  - The fibonacci number consed is the fibonacci number after the next fibonacci number after the largest fibonacci number in the list being consed onto.
+       - This can be seen in the line: let part2 := map (fun xs => fib (n2 + 3) :: xs) (zeck_lists n2) in
+  
+  3.
+  Every integer has a zeckendorf representation because every iteration simply re-adds the representations from two iterations ago plus exactly the least number required to not repeat an existing total.
+*)
+
 Fixpoint find_fib_index_aux (n k b : nat) : nat :=
   match b with
   | 0 => k
